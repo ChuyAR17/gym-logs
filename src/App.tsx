61ceac6b40049b2f3956df77b3e7c80178937f6c
Jesus Ramirez <jar170396@gmail.com>
2022-05-20 22:25:27 -0700

    feat(App file): Added initial structure.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GiWeightLiftingUp } from 'react-icons/gi';
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <h1>This is my Gym Logs App</h1>
+      <hr />
+      <p>Here goes all the code</p>
+      <GiWeightLiftingUp size='200px' />
+    </div>
+  );
+}
